db5982a8fe2cdc47152934feccd98ddf86172af7
William Gu <williamgu2011@hotmail.com>
2018-03-01 09:13:20 -0800

    Added callibration to speed setter method

--- PF1/PrincipleFunction1/PrincipleFunction1.ino
+++ PF1/PrincipleFunction1/PrincipleFunction1.ino
@@ -29,9 +29,7 @@ const int LEFT_HE_PIN = 3;
 double leftLastMillis;
 double rightLastMillis;
 
-double distToCenter = 0.031;
-volatile byte left_half_revolutions = 0;
-volatile byte right_half_revolutions = 0;
+double distToCenter = 3;
 
 const int LCD_RS = 0;
 const int LCD_EN = 1;
@@ -169,8 +167,19 @@ void setForwardSpeed(int speed){
   }
   digitalWrite(MOTOR_POLARITY_PIN1, LOW);
   digitalWrite(MOTOR_POLARITY_PIN2, HIGH);
+
+  //Calibrate:
   analogWrite(MOTOR_POWER_PIN1, speed);
   analogWrite(MOTOR_POWER_PIN2, speed);
+  while (leftTireSpeed<rightTireSpeed){
+    analogWrite(MOTOR_POWER_PIN2, speed-=5);
+    delay(100);
+  }
+  while (rightTireSpeed<leftTireSpeed){
+    analogWrite(MOTOR_POWER_PIN1, speed-=5);
+    delay(100);
+  }
+  
   currSpeed = speed;
   //updateLCD();
 }
@@ -272,25 +281,17 @@ float receiveHCSR04(int echoPin){
 }
 
 void updateLeftHE() {
-  left_half_revolutions++;
-  if (left_half_revolutions >= 20) {
-    double timeChange = millis() - leftLastMillis;
-    leftLastMillis = millis();
-    String outputMessage = "Left Tire speed is " + String(calcTireSpeed(timeChange)*left_half_revolutions) + " m/s";
-    left_half_revolutions = 0;
-      Serial.println(outputMessage);
-  }
+  double timeChange = millis() - leftLastMillis;
+  leftLastMillis = millis();
+  Serial.println(calcTireSpeed(timeChange));
+  leftTireSpeed = calcTireSpeed(timeChange);
 }
 
 void updateRightHE() {
-  right_half_revolutions++;
-  if (right_half_revolutions >= 20) {
-    double timeChange = millis() - rightLastMillis;
-    rightLastMillis = millis();
-    String outputMessage = "Right Tire speed is " + String(calcTireSpeed(timeChange)*right_half_revolutions) + "  m/s";
-    right_half_revolutions = 0;
-    Serial.println(outputMessage);
-  }
+  double timeChange = millis() - rightLastMillis;
+  rightLastMillis = millis();
+  Serial.println(calcTireSpeed(timeChange));
+  rightTireSpeed = calcTireSpeed(timeChange);
 }
 
 double calcTireSpeed(double time) {
