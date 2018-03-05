36cdedfad494ea759c07246ce5de7e3d6b20d020
William Gu <williamgu2011@hotmail.com>
2018-03-04 18:56:57 -0800

    Minor formatting changes

--- FINAL_ARDUINO/FINAL_ARDUINO.ino
+++ FINAL_ARDUINO/FINAL_ARDUINO.ino
@@ -12,7 +12,7 @@ int servoPos = 90; // variable to store the servo position (90 deg is middle pos
 unsigned long timer = 0;
 
 /* PF2 stuff */
-int opticalSensors[] = {0,0,0,0};
+int opticalSensors[] = {0, 0, 0, 0};
 int THRESHOLD = 500;
 
 /* Constant Pin Assignments */
@@ -61,12 +61,12 @@ void setup() {
   Serial.begin(9600);
   pinMode(LM35_PIN, INPUT);
   pinMode(HC_SR04_TRIG_PIN, OUTPUT);
-  pinMode(HC_SR04_ECHO_PIN, INPUT); 
+  pinMode(HC_SR04_ECHO_PIN, INPUT);
   myservo.attach(SERVO_PIN);
   myservo.write(servoPos); //set servo position to mid
   pinMode(MOTOR_POLARITY_PIN1, OUTPUT); //Direction
   pinMode(MOTOR_POLARITY_PIN2, OUTPUT); //Direction
-  
+
   pinMode(RIGHT_HE_PIN, INPUT_PULLUP);
   pinMode(LEFT_HE_PIN, INPUT_PULLUP);
 
@@ -78,7 +78,7 @@ void setup() {
 
   lcd.begin(16, 2); //Setup LCD num of cols and rows
   updateLCD();
-  
+
   // Set initial rotation speed to 0
   analogWrite(MOTOR_POWER_PIN1, 0);
   //digitalWrite(MOTOR_POLARITY_PIN1, LOW);
@@ -87,41 +87,41 @@ void setup() {
 
   //check dip switch status and set mode appropriately. Only DIP_1 on for PF1, Only DIP_2 on for PF2, Only DIP_3 on for AF
   //error check for unsupported dip switch input
-  while(digitalRead(DIP_1) && digitalRead(DIP_2) && digitalRead(DIP_3) 
-    || digitalRead(DIP_1) && digitalRead(DIP_2) 
-    || digitalRead(DIP_2) && digitalRead(DIP_3) 
-    || digitalRead(DIP_1) && digitalRead(DIP_3)){
-      //TODO print error message to LCD
-      
-      delay(500);
-    }
+  while (digitalRead(DIP_1) && digitalRead(DIP_2) && digitalRead(DIP_3)
+         || digitalRead(DIP_1) && digitalRead(DIP_2)
+         || digitalRead(DIP_2) && digitalRead(DIP_3)
+         || digitalRead(DIP_1) && digitalRead(DIP_3)) {
+    //TODO print error message to LCD
 
-    if(digitalRead(DIP_1)){
-      state = MODE_PF1;
-    }
-    else if(digitalRead(DIP_2){
-      state = MODE_PF2;
-    }
-    else{
-      state = MODE_AF;
-    }
+    delay(500);
+  }
 
-  //Put delay to start? 
+  if (digitalRead(DIP_1)) {
+    state = MODE_PF1;
+  }
+  else if (digitalRead(DIP_2) {
+  state = MODE_PF2;
+}
+else {
+  state = MODE_AF;
+}
+
+//Put delay to start?
 }
 
 void loop() {
   /*
-  myservo.write(0);
-  delay(2500);
-  myservo.write(180);
-  delay(2500);*/
-  switch(state){
+    myservo.write(0);
+    delay(2500);
+    myservo.write(180);
+    delay(2500);*/
+  switch (state) {
     case PF1: principleFunction1();
     case PF2: principleFunction2();
     case AF: AF();
-  }  
+  }
   /*
-  for (int i=0; i<200; i+=5){
+    for (int i=0; i<200; i+=5){
     setForwardSpeed(i);
     Serial.println(getLMTemp(LM35_PIN));
     if (i<100){
@@ -131,18 +131,18 @@ void loop() {
       myservo.write(180);
     }
     delay(250);
-  }*/
+    }*/
 }
 
 /**
- * Initiate autonomous procedure for principle function 1
- * Robot moves forward at max speed until it detects an object within 50 cm
- * Robot gradually slows down and stops before hitting the wall (as close as pos.)
- * Robot checks left and right using servo and then chooses side with most space and repeats
- */
-void principleFunction1(){
-  while (getDist()>10){
-    if (currDist > 50){
+   Initiate autonomous procedure for principle function 1
+   Robot moves forward at max speed until it detects an object within 50 cm
+   Robot gradually slows down and stops before hitting the wall (as close as pos.)
+   Robot checks left and right using servo and then chooses side with most space and repeats
+*/
+void principleFunction1() {
+  while (getDist() > 10) {
+    if (currDist > 50) {
       //Max speed, set currSpeed
       //Serial.print("Dist:");
       //Serial.println(currDist);
@@ -151,9 +151,9 @@ void principleFunction1(){
     else if (currDist > 10) {
       //Serial.print("Dist:");
       //Serial.println(currDist);
-    
+
       //Change speed as a function of distance
-      int forwardSpeed = 255.0 - currDist*25.5;
+      int forwardSpeed = 255.0 - currDist * 25.5;
       Serial.print("SPEED: ");
       Serial.println(forwardSpeed);
       setForwardSpeed(forwardSpeed);
@@ -167,12 +167,12 @@ void principleFunction1(){
   float rightDist = getDist();
   myservo.write(90); //Reset position
 
-  if (leftDist>rightDist){
+  if (leftDist > rightDist) {
     Serial.println("LEFT IS CLEAR");
     stationaryLeftTurn();
     //Turn left 90 degrees
   }
-  else{
+  else {
     Serial.println("RIGHT IS CLEAR");
     stationaryRightTurn();
     //Turn right 90 degrees
@@ -180,16 +180,16 @@ void principleFunction1(){
 }
 
 /*
- * Principle function 2 allows robot to track and follow black electrical tape laid on the ground
- */
+   Principle function 2 allows robot to track and follow black electrical tape laid on the ground
+*/
 void principleFunction2() {
   updateOpticalSensors();
   updateDrive();
 }
 
 /*
- * Updates PF2 motor speeds depending on current optical sensor readings
- */
+   Updates PF2 motor speeds depending on current optical sensor readings
+*/
 void updateDrive() {
   if (!opticalSensors[0] && !opticalSensors[1]) {
     setForwardSpeed(255);
@@ -203,10 +203,10 @@ void updateDrive() {
   }
 }
 
-/* 
- * Reads the 4 optical sensors and updates sensors array  
- */
-void updateOpticalSensors() { 
+/*
+   Reads the 4 optical sensors and updates sensors array
+*/
+void updateOpticalSensors() {
 
   if (analogRead(OPTICAL_SENSOR_PIN0) > THRESHOLD) {
     opticalSensors[0] = 1;
@@ -222,13 +222,13 @@ void updateOpticalSensors() {
 }
 
 /**
- * Updates the LCD display reading given the current speed of the robot (in the member variable)
- * and the mode it's in
- */
-void updateLCD(){
-  if (millis()-timer < 100)
+   Updates the LCD display reading given the current speed of the robot (in the member variable)
+   and the mode it's in
+*/
+void updateLCD() {
+  if (millis() - timer < 100)
     return; //Do not update more than once a second
-    
+
   timer = millis();
   lcd.clear();
   lcd.print("DIST: ");
@@ -241,12 +241,12 @@ void updateLCD(){
 }
 
 /**
- * Turns motors such that both wheels go in forward direction
- * Changes currspeed to speed that we set forward speed to
- * Speed should be an int between 0 and 255
- */
-void setForwardSpeed(int speed){
-  if (speed<0){
+   Turns motors such that both wheels go in forward direction
+   Changes currspeed to speed that we set forward speed to
+   Speed should be an int between 0 and 255
+*/
+void setForwardSpeed(int speed) {
+  if (speed < 0) {
     speed = 0;
   }
   digitalWrite(MOTOR_POLARITY_PIN1, LOW);
@@ -255,24 +255,24 @@ void setForwardSpeed(int speed){
   //Calibrate:
   analogWrite(MOTOR_POWER_PIN1, speed);
   analogWrite(MOTOR_POWER_PIN2, speed);
-  while (leftTireSpeed<rightTireSpeed){
-    analogWrite(MOTOR_POWER_PIN2, speed-=5);
+  while (leftTireSpeed < rightTireSpeed) {
+    analogWrite(MOTOR_POWER_PIN2, speed -= 5);
     delay(50);
   }
-  while (rightTireSpeed<leftTireSpeed){
-    analogWrite(MOTOR_POWER_PIN1, speed-=5);
+  while (rightTireSpeed < leftTireSpeed) {
+    analogWrite(MOTOR_POWER_PIN1, speed -= 5);
     delay(50);
   }
-  
+
   currSpeed = speed;
   //updateLCD();
 }
 
 /*
- * Turns arduino left while setting right motor speed to speed
- */
-void movingLeftTurn(int speed){
-  if (speed<0){
+   Turns arduino left while setting right motor speed to speed
+*/
+void movingLeftTurn(int speed) {
+  if (speed < 0) {
     speed = 0;
   }
   digitalWrite(MOTOR_POLARITY_PIN1, LOW);
@@ -280,18 +280,18 @@ void movingLeftTurn(int speed){
 
   //May need calibration
   analogWrite(MOTOR_POWER_PIN1, speed);
-  if (speed-100<0)
-      analogWrite(MOTOR_POWER_PIN2, 0);
+  if (speed - 100 < 0)
+    analogWrite(MOTOR_POWER_PIN2, 0);
   else
-    analogWrite(MOTOR_POWER_PIN2, speed-100);
+    analogWrite(MOTOR_POWER_PIN2, speed - 100);
   currSpeed = speed;
 }
 
 /*
- * Turns arduino right while setting left motor speed to speed
- */
-void movingRightTurn(int speed){
-  if (speed<0){
+   Turns arduino right while setting left motor speed to speed
+*/
+void movingRightTurn(int speed) {
+  if (speed < 0) {
     speed = 0;
   }
   digitalWrite(MOTOR_POLARITY_PIN1, LOW);
@@ -299,54 +299,54 @@ void movingRightTurn(int speed){
 
   //May need calibration
   analogWrite(MOTOR_POWER_PIN2, speed);
-  if (speed-100<0)
-      analogWrite(MOTOR_POWER_PIN1, 0);
+  if (speed - 100 < 0)
+    analogWrite(MOTOR_POWER_PIN1, 0);
   else
-    analogWrite(MOTOR_POWER_PIN1, speed-100);
+    analogWrite(MOTOR_POWER_PIN1, speed - 100);
   currSpeed = speed;
 }
 
 /**
- * Turns motors such that the whole robot turns left without moving forward
- * Changes currspeed to 0
- */
-void stationaryLeftTurn(){
+   Turns motors such that the whole robot turns left without moving forward
+   Changes currspeed to 0
+*/
+void stationaryLeftTurn() {
   currSpeed = 0;
   //updateLCD();
-  
+
   digitalWrite(MOTOR_POLARITY_PIN1, LOW);
   digitalWrite(MOTOR_POLARITY_PIN2, LOW);
   analogWrite(MOTOR_POWER_PIN1, 255); //Left wheel
   analogWrite(MOTOR_POWER_PIN2, 255); //Right wheel
   delay(5000);
-  
+
   analogWrite(MOTOR_POWER_PIN1, 0);
   analogWrite(MOTOR_POWER_PIN2, 0);
 }
 
 /**
- * Turns motors such that the whole robot turns right without moving forward
- * Changes currspeed to 0
- */
-void stationaryRightTurn(){
+   Turns motors such that the whole robot turns right without moving forward
+   Changes currspeed to 0
+*/
+void stationaryRightTurn() {
   currSpeed = 0;
   //updateLCD();
-  
+
   digitalWrite(MOTOR_POLARITY_PIN1, LOW);
   digitalWrite(MOTOR_POLARITY_PIN2, LOW);
   analogWrite(MOTOR_POWER_PIN1, 255);
   analogWrite(MOTOR_POWER_PIN2, 255);
-  delay(5000); 
-    
+  delay(5000);
+
   analogWrite(MOTOR_POWER_PIN1, 0);
   analogWrite(MOTOR_POWER_PIN2, 0);
 }
 
 /**
- * Returns the distance reading from the HC SR04 in cm
- * Also modifies member variables: currTemp and speedSound and currDist
- */
-float getDist(){
+   Returns the distance reading from the HC SR04 in cm
+   Also modifies member variables: currTemp and speedSound and currDist
+*/
+float getDist() {
   currTemp = getLMTemp(LM35_PIN);
   speedSound = 331.5 + (0.6 * currTemp);
   currDist = readHCSR04(HC_SR04_TRIG_PIN, HC_SR04_ECHO_PIN);
@@ -354,26 +354,26 @@ float getDist(){
 }
 
 /**
- * Get the temperature reading from the LM35 (in degrees celsius)
- */
-float getLMTemp(int PIN){
+   Get the temperature reading from the LM35 (in degrees celsius)
+*/
+float getLMTemp(int PIN) {
   return (5.0 * analogRead(PIN) * 100.0 / 1024);
 }
 
 /**
- * Initiates and takes a reading from the HC-SR04.
- * Returns a distance to detected object in cm.
- * If no object is detected returns 400 (max range of device)
- */
-float readHCSR04(int trigPin, int echoPin){
+   Initiates and takes a reading from the HC-SR04.
+   Returns a distance to detected object in cm.
+   If no object is detected returns 400 (max range of device)
+*/
+float readHCSR04(int trigPin, int echoPin) {
   initiateHCSR04(trigPin);
   return receiveHCSR04(echoPin);
 }
 
 /**
- * Procedure to initiate HC SR04 sensor reading
- */
-void initiateHCSR04(int trigPin){  
+   Procedure to initiate HC SR04 sensor reading
+*/
+void initiateHCSR04(int trigPin) {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
 
@@ -384,21 +384,21 @@ void initiateHCSR04(int trigPin){
 }
 
 /**
- * Procedure to receive response from HC SR04 sensor reading and return distance in cm
- * Will return -1 if no obstacle is detected
- */
-float receiveHCSR04(int echoPin){
-  unsigned long pulseDuration; 
+   Procedure to receive response from HC SR04 sensor reading and return distance in cm
+   Will return -1 if no obstacle is detected
+*/
+float receiveHCSR04(int echoPin) {
+  unsigned long pulseDuration;
   unsigned long timeOutDuration;
 
   timeOutDuration = 36000;
   pulseDuration = pulseIn(echoPin, HIGH, timeOutDuration);
 
-  if (pulseDuration == 0){ //If echo pulse times out, no obstacle is detected
+  if (pulseDuration == 0) { //If echo pulse times out, no obstacle is detected
     return 400;
   }
   else {
-    return pulseDuration/(20000.0/speedSound); //Return distance otherwise
+    return pulseDuration / (20000.0 / speedSound); //Return distance otherwise
   }
 }
 
@@ -417,6 +417,6 @@ void updateRightHE() {
 }
 
 double calcTireSpeed(double time) {
-  return (distToCenter * PI) / (time/1000); 
+  return (distToCenter * PI) / (time / 1000);
 }
 
